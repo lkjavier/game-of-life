cdfa0037057272c53a58ae819540342839a2957e
Guido <guido@millenaar.net>
2018-05-02 16:22:13 +0200

    template and api

--- cell_api.py
+++ cell_api.py
@@ -1,15 +1,21 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 from CellGrid import CellGrid
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
-def advance_state():
+def start():
     if request.method == 'GET':
         grid = CellGrid()
-        return grid.get_json_state()
     else:
         grid = CellGrid(request.data)
-        grid.advance()
-        return grid.get_json_state()
+        grid.advance() 
+    return render_template('index.html', state=grid.state)
+
+@app.route('/advance', methods=['POST'])
+def advance():
+    grid= CellGrid(request.data)
+    grid.advance()
+    return grid.get_json_state()
