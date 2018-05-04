fc825e6eccb7dd264919b8cf82403e0e9fe97270
Guido <guido@millenaar.net>
2018-05-04 15:12:55 +0200

    Added SocketIO

--- cell_api.py
+++ cell_api.py
@@ -1,9 +1,11 @@
-from flask import Flask
-from flask import request
-from flask import render_template
+from flask import Flask, request, render_template
+from flask_socketio import SocketIO, send, emit
+import json
 from CellGrid import CellGrid
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/', methods=['GET','POST'])
 def start():
@@ -19,3 +21,12 @@ def advance():
     grid = CellGrid(request.data)
     grid.advance()
     return grid.get_json_state()
+
+@socketio.on('Call')
+def handle_message(message):
+    print(json.dumps(message))
+    emit('Response', [['Response']])
+    #pass
+
+if __name__ == '__main__':
+    socketio.run(app)
