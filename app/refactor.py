e8ed7138ee4e9884f96796b1ccd8bec480634130
jelle <jellemunk@gmail.com>
2018-05-12 12:41:31 +0200

    Fix CellGrid class in refactor and add doc tests

--- app/refactor.py
+++ app/refactor.py
@@ -1,29 +1,39 @@
 '''
 Refactor of CellGrid.py
 '''
-import copy
-import json
-import time
 import random
-
-
-
+import importlib
+import numpy as np
 
 class CellGrid:
     '''
     CellGrid Object
+    >>> grid = CellGrid(size = 20)
+    >>> grid.size()
+    20
+    >>> grid.out_of_bounds(20)
+    True
+    >>> grid.out_of_bounds(19)
+    False
+    >>> grid.set_cell(5,5)
+    >>> grid.state[5][5]
+    1
+    >>> sum(sum(np.array(grid.state)))
+    1
+    >>> grid.randomize()
+    >>> sum(sum(np.array(grid.state))) > 50
+    True
+    >>> sum(sum(np.array(grid.state))) < 150
+    True
     '''
     def __init__(self, state = None, size = 30):
-         if state == None:
+        if state == None:
             self.state = [[0 for i in range(0, size)] for i in range(0, size)]
         else:
             self.state = state
 
-    def randomize(self):
-        n = 0
-        while n < self.size()*self.size() / 4 :
-            self.set_cell(random.randint(0, self.size() -1), random.randint(0,self.size() -1))
-            n += 1
+    def randomize(self, prob = 0.25):
+        self.state = [[ int(random.random() < prob) for i in range(0, self.size())] for i in range(0, self.size())]
     
     def size(self):
         return len(self.state)
@@ -39,9 +49,9 @@ class CellGrid:
         return False
     
     def advance(self, rule):
-        new_state = [[0 for i in range(0, size)] for i in range(0, size)]
-        for _, x : self.size():
-            for _, y : self.size():
+        new_state = [[0 for i in range(0, self.size())] for i in range(0, self.size())]
+        for x in range(0, self.size() - 1):
+            for y in range(0, self.size() - 1):
                 new_state[x][y] = rule.run(self.state, x, y)
         self.state = new_state 
 
@@ -64,14 +74,17 @@ class ConwayRule(Rule):
         return 1        
 
 class GridGame():
-    
-    def __init__(self, size = 100, rulename = "Conway"):
-        self.cellGrid = new CellGrid(size = size)
-        self.rule = new ConwayRule()
+    '''
 
-    def start()
-        cellGrid.randomize()
-        for t in range(1,1000):
-            cellGrid.advance(rule)
+    '''
+    def __init__(self, size = 100, ruleName = "Conway"):
+        self.cellGrid = CellGrid(size = size)
+        MyRule = getattr(importlib.import_module("module.submodule"), ruleName + "Rule")
+        self.rule = MyRule()
+
+    def start(self):
+        self.cellGrid.randomize()
+        #for t in range(1,1000):
+        self.cellGrid.advance(self.rule)
 
     
