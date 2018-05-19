01d34e0befe77dc783690628e5ee975ae5e2da68
jelle <jellemunk@gmail.com>
2018-05-19 17:15:01 +0200

    Add docker compose

---
+++ kafka_producer.py
@@ -0,0 +1,9 @@
+from kafka import KafkaProducer
+import json
+producer = KafkaProducer(bootstrap_servers='kafka:9092',
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+                         api_version=(0, 10, 1))
+for i in range(0, 100):
+	producer.send("gameoflife", {"message":"test" + str(i)})
+producer.flush()
+print("done with producing")
